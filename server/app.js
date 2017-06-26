6dbc30da13e98665f8a0e2527ac1d064917ca5b7
Dagzy <quincye.williams@gmail.com>
2017-06-25 23:58:55 -0400

    QW refactored and added routing

--- server/app.js
+++ server/app.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
-var sequelize = require('./db.js')
-var User = sequelize.import('./models/user');
+var sequelize = require('./db')
+var User = sequelize.import(__dirname +'\\models\\user');
 
 User.sync();  //User.sync({ force: true }); WARNING: THIS WILL DROP A TABLE!
 
@@ -10,31 +10,11 @@ User.sync();  //User.sync({ force: true }); WARNING: THIS WILL DROP A TABLE!
 
 app.use(bodyParser.json());
 app.use(require('./middleware/headers'));
-
+app.use('/api/user', require('./routes/user'));
 app.use('/api/test', function(req, res){
 	res.send("Hello World");
 });
 
 app.listen(3000, function(){
 	console.log("app is listening on port 3000");
-});
-app.post('/api/user', function(req,res){
-	var username = req.body.user.username;
-	var pass = req.body.user.password;
-	User.create({
-		username: username,
-		passwordhash : ''
-	}).then(
-			//Sequelize is going to return the object it created from the db.
-			function createSuccess(user){
-				res.json({
-					user: user,
-					message: 'create'
-				});
-			},
-			function createError(err){
-				res.send(500, err.message);
-			}
-		);
-	//Need to create a user object and use Sequelize to put that user into our database
 });
